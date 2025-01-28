1bd9fe5552be63124a1b7472099165f51f22f3fc
Hafsa Egal <hafsaegal915@gmail.com>
2025-01-28 08:07:54 +0300

    calculated day of the week

--- script/script.js
+++ script/script.js
@@ -29,5 +29,22 @@ const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   }
 
  }
+
+ //calculate the day of the week the user was born
+
+ function calculateDayOfWeek(day, month, year) {
+   const CC = Math.floor(year / 100);
+   const YY = year % 100;
+
+   const dayOfWeek =
+     (Math.floor(CC / 4) -
+       2 * CC +
+       YY +
+       Math.floor(YY / 4) +
+       Math.floor((26 * (month + 1)) / 10) +
+       day) %
+     7;
+   return (dayOfWeek + 7) % 7; // Ensure the result is always positive
+ }
   
 
